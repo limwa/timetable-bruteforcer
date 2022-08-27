a9296029015bfdde5bfb5c142d885e643a2de41b
André Lima <me@limwa.pt>
2022-08-27 01:51:28 +0100

    refactor: be consistent with url casing

--- src/fetcher/search/teachers/api/mod.ts
+++ src/fetcher/search/teachers/api/mod.ts
@@ -3,7 +3,7 @@ import { withAuthentication } from "../../../cookies.ts";
 import parseSearchResults from "../../../../parser/search/teachers/api/mod.ts";
 
 export function getTeacherFromAbbreviation(abbreviation: string) {
-    const url = new URL("https://sigarra.up.pt/feup/pt/MOB_func_GERAL.pesquisa");
+    const url = new URL("https://sigarra.up.pt/feup/pt/MOB_FUNC_GERAL.PESQUISA");
     url.searchParams.set("pv_sigla", abbreviation);
 
     return withAuthentication(url.toString())

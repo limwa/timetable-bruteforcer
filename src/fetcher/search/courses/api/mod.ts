c4e1f82a13e84898c303047559b7aa8284c4a7a5
André Lima <me@limwa.pt>
2023-01-29 15:42:56 +0000

    feat: search for all curricular units with abbreviation

--- src/fetcher/search/courses/api/mod.ts
+++ src/fetcher/search/courses/api/mod.ts
@@ -1,11 +1,27 @@
 import { withAuthentication } from "../../../cookies.ts";
 import parseSearchResults from "../../../../parser/search/courses/api/mod.ts";
+import { Course } from "../../../../parser/search/courses/types.d.ts";
 
-export function getCoursesByAbreviation(abbreviation: string) {
-    const url = new URL("https://sigarra.up.pt/feup/pt/MOB_UCURR_GERAL.PESQUISA");
-    url.searchParams.set("pv_uc_sigla", abbreviation);
+export async function getCoursesByAbreviation(abbreviation: string) {
+    const courses: Course[] = [];
 
-    return withAuthentication(url.toString())
-        .then((response) => response.text())
-        .then(parseSearchResults);
+    let page = 1;
+    while (true) {
+        const url = new URL("https://sigarra.up.pt/feup/pt/MOB_UCURR_GERAL.PESQUISA");
+        url.searchParams.set("pv_uc_sigla", abbreviation);
+        url.searchParams.set("pv_pag", page.toString(10));
+
+        const pageCourses = await withAuthentication(url.toString())
+            .then((response) => response.text())
+            .then(parseSearchResults);
+
+        courses.push(...pageCourses);
+        page++;
+        
+        if (pageCourses.length === 0) {
+            break;
+        }
+    }
+
+    return courses;
 }
